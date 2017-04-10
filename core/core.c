e2d4957f0111e28cfca0e60a1c6ee54b52ff82b6
Bredian <sasha.kry@gmail.com>
2017-04-10 22:15:11 +0300

    Update core.c

--- core/core.c
+++ core/core.c
@@ -1,3 +1,21 @@
 #include <avr/io.h>
 #include <stdlib.h>
 #include <math.h>
+#include "bomb.h"
+
+int loop(){
+	
+}
+
+int setup(){
+	
+}
+
+
+
+int main(){
+	setup();
+	while(1){
+		loop();
+	}
+}
