e2c4ccff99254d2b1e870e4ac9faa7fb35525b37
Bredian <sasha.kry@gmail.com>
2017-04-11 11:54:48 +0400

    Update core.c

--- core/core.c
+++ core/core.c
@@ -10,7 +10,14 @@ int setup(){
 }
 
 int loop(){
-	
+	/*7 Segment Level Show Here*/
+	check_gameover();
+	/*Controls Function Here*/
+	/*Sucsessful Turn Sound Here*/
+	proceed_turn(level);
+	if(bp==1 && bx!=-1 && by!=-1 && bc!=0 ) bomb_explosion();
+	check_nish();
+	/*Printing Map Function Here*/
 }
 
 int main(){
