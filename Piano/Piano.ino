75edf26422d8e9dcde40868f0fe82ada4fd1bc27
fedosgad <fedosgad@gmail.com>
2017-10-21 21:33:45 +0300

    splitted notes[][] array

--- Piano/Piano.ino
+++ Piano/Piano.ino
@@ -29,10 +29,9 @@ PS2Keyboard keyboard;
 #define KEYS 29
 
 // notes to play, corresponding to the 3 sensors:
-int notes[2][KEYS] = {
-  {'q','2','w','3','e','r','5','t','6','y','7','u','i','9','o','0','p','z','s','x','d','c','f','v','b','h','n','j','m'},
-  {NOTE_C3,NOTE_CS3,NOTE_D3,NOTE_DS3,NOTE_E3,NOTE_F3,NOTE_FS3,NOTE_G3,NOTE_GS3,NOTE_A3,NOTE_AS3,NOTE_B3,NOTE_C4,NOTE_CS4,NOTE_D4,NOTE_DS4,NOTE_E4,NOTE_F4,NOTE_FS4,NOTE_G4,NOTE_GS4,NOTE_A4,NOTE_AS4,NOTE_B4,NOTE_C5,NOTE_CS5,NOTE_D5,NOTE_DS5,NOTE_E5}
-};
+const char buttons = {'q','2','w','3','e','r','5','t','6','y','7','u','i','9','o','0','p','z','s','x','d','c','f','v','b','h','n','j','m'};
+
+const int notes[KEYS] = {NOTE_C3,NOTE_CS3,NOTE_D3,NOTE_DS3,NOTE_E3,NOTE_F3,NOTE_FS3,NOTE_G3,NOTE_GS3,NOTE_A3,NOTE_AS3,NOTE_B3,NOTE_C4,NOTE_CS4,NOTE_D4,NOTE_DS4,NOTE_E4,NOTE_F4,NOTE_FS4,NOTE_G4,NOTE_GS4,NOTE_A4,NOTE_AS4,NOTE_B4,NOTE_C5,NOTE_CS5,NOTE_D5,NOTE_DS5,NOTE_E5}
 
 char keys[10];
 
@@ -43,16 +42,16 @@ void piano_play() {
   
   for(int j = 0; j < S; j++) {
     for(int i = 0; i < KEYS; i++) {
-      if(keys[j]==notes[0][i])
+      if(keys[j] == buttons[i])
        qa[i] = i;
     }
   }
   
   if(S==1)
-   tone(7, notes[1][qa[0]], 20);
+   tone(7, notes[qa[0]], 20);
   else if( S > 0 ) {
     for(int i = 0; i < S; i++) {
-      tone(7, notes[1][qa[i]], 20);
+      tone(7, notes[qa[i]], 20);
       delay(20);
       noTone(7);
     }
